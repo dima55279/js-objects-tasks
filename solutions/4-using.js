42ec9cecc43ffde77846977f8229138dc6e4cf5b
dima55279 <dima55279@yandex.ru>
2025-03-11 16:04:03 +0300

    Выполнено задание 4.

--- solutions/4-using.js
+++ solutions/4-using.js
@@ -1,3 +1,14 @@
 // BEGIN
-
+const getDomainInfo = (site) => {
+    const infoAboutSite = {
+        scheme: "http",
+        name: "",
+    };
+    if (site.startsWith('https')) {
+        infoAboutSite.scheme = 'https';
+    }
+    infoAboutSite.name = site.replace(infoAboutSite.scheme + "://", "");
+    return infoAboutSite;
+}
+export default getDomainInfo;
 // END
