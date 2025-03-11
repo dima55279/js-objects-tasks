e9042304f5bf257265d7d7f2f6d3175482d8b084
dima55279 <dima55279@yandex.ru>
2025-03-11 16:03:49 +0300

    Выполнено задание 3.

--- solutions/3-references.js
+++ solutions/3-references.js
@@ -1,3 +1,11 @@
 // BEGIN
-
+const is = (firstObject, secondObject) => {
+    if (firstObject.name === secondObject.name
+        && firstObject.state === secondObject.state
+        && firstObject.website === secondObject.website) {
+        return true;
+    }
+    return false;
+}
+export default is;
 // END
