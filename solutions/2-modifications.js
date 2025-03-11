8926a4c0cddfa9c8f673160b6659b4de702a20aa
dima55279 <dima55279@yandex.ru>
2025-03-11 16:02:41 +0300

    Выполнено задание 2.

--- solutions/2-modifications.js
+++ solutions/2-modifications.js
@@ -1,5 +1,10 @@
 import _ from 'lodash';
 
 // BEGIN
-
-  // END
+const normalize = (obj) => {
+    obj.name = _.capitalize(obj.name);
+    obj.description = obj.description.toLowerCase();
+    return obj;
+}
+export default normalize;
+// END
