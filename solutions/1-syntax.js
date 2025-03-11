b78a05d4c758fa6579e5a55c30c813fbcfbc828d
dima55279 <dima55279@yandex.ru>
2025-03-11 16:01:57 +0300

    Выполнено задание 1.

--- solutions/1-syntax.js
+++ solutions/1-syntax.js
@@ -1,3 +1,12 @@
 // BEGIN
-
+const getJsonFileData = () => {
+    const obj = {
+        "files": [
+            "src/objects.js"
+        ],
+        "config": true,
+    }
+    return obj
+}
+export default getJsonFileData;
 // END
