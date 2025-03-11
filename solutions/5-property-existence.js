db07be3fb8510567ad329786739d1a5f2e640e0e
dima55279 <dima55279@yandex.ru>
2025-03-11 16:04:16 +0300

    Выполнено задание 5.

--- solutions/5-property-existence.js
+++ solutions/5-property-existence.js
@@ -1,5 +1,16 @@
 import _ from 'lodash';
 
 // BEGIN
-
+const countWords = (sentence) => {
+    const words = _.words(sentence.toLowerCase());
+    const countObject = {};
+    for (const word of words) {
+        if (countObject[word] === undefined) {
+            countObject[word] = 1;
+        }
+        else countObject[word] += 1;
+    }
+    return countObject;
+}
+export default countWords;
 // END
