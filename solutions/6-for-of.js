091dc6b17e2a4d56d8c8a1b4ec9be5428c5bb79b
dima55279 <dima55279@yandex.ru>
2025-03-12 11:44:14 +0300

    Выполнено задание 6.

--- solutions/6-for-of.js
+++ solutions/6-for-of.js
@@ -1,3 +1,12 @@
 // BEGIN
-
+const pick = (obj, properties) => {
+    const newObject = {};
+    for (const property of properties) {
+        if (obj[property] !== undefined) {
+            newObject[property] = obj[property];
+        }
+    }
+    return newObject;
+}
+export default pick;
 // END
