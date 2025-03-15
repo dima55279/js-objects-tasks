f8f28ef2eaf832a82893f952d4206180b7d42954
dima55279 <dima55279@yandex.ru>
2025-03-15 17:32:11 +0300

    Выполнено задание 9.

--- solutions/9-cloning.js
+++ solutions/9-cloning.js
@@ -1,3 +1,10 @@
 // BEGIN
-
+const cloneShallow = (obj) => {
+    const cloneObject = {};
+    for (const props in obj) {
+        cloneObject[props] = obj[props];
+    }
+    return cloneObject;
+}
+export default cloneShallow;
 // END
