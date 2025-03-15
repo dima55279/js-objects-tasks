3a6001b301d6e4e60ed361287dae438b55181f6b
dima55279 <dima55279@yandex.ru>
2025-03-16 02:16:45 +0300

    Выполнено задание 11.

--- solutions/11-destructuring.js
+++ solutions/11-destructuring.js
@@ -1,3 +1,11 @@
 // BEGIN
-
+const getSortedNames = (arr) => {
+    const namesArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        const { name } = arr[i];
+        namesArray.push(name);
+    }
+    return namesArray.sort();
+}
+export default getSortedNames;
 // END
