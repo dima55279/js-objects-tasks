b3a6da33855de5efec9fefcc33db0a0dd1b4a8ea
dima55279 <dima55279@yandex.ru>
2025-03-16 02:11:44 +0300

    Выполнено задание 10.

--- solutions/10-spread-operator.js
+++ solutions/10-spread-operator.js
@@ -1,3 +1,15 @@
 // BEGIN
-
+const make = (companyName, obj = {}) => {
+    const companyObject = {name: companyName, ...obj};
+    if (Object.hasOwn(obj, 'state') === false) {
+        companyObject.state = "moderating";
+    }
+    if (Object.hasOwn(obj, 'createdAt') === false) {
+        companyObject.createdAt = Date.now();
+    }
+    return companyObject;
+}
+const company = make('Yandex');
+console.log(company);
+export default make;
 // END
