1c4359678b49d697169609d985e234460c912636
dima55279 <dima55279@yandex.ru>
2025-03-15 17:08:02 +0300

    Выполнено задание 7.

--- solutions/7-nested-objects.js
+++ solutions/7-nested-objects.js
@@ -1,3 +1,26 @@
 // BEGIN
-
+const get = (obj, keys) => {
+    const newObj = {};
+    if (keys.length > 1) {
+        for (let i = 0; i < keys.length - 1; i++) {
+            if (Object.hasOwn(obj, keys[i])) {
+                const temp = {...obj[keys[i]]};
+                for (const props in temp) {
+                    newObj[props] = temp[props];
+                }
+            }
+            if (Object.hasOwn(newObj, keys[i])) {
+                const temp = {...newObj[keys[i]]};
+                for (const props in temp) {
+                    newObj[props] = temp[props];
+                }
+            }
+        }
+    }
+    else if (Object.hasOwn(obj, keys[0]))
+        newObj[keys[0]] = obj[keys[0]];
+    if (Object.hasOwn(newObj, keys[keys.length - 1])) return newObj[keys[keys.length - 1]];
+    else return null;
+}
+export default get;
 // END
