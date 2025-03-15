6a470731c799e78b6a39cc1c4522abc3cc6854f6
dima55279 <dima55279@yandex.ru>
2025-03-15 17:27:05 +0300

    Выполнено задание 8.

--- solutions/8-merging.js
+++ solutions/8-merging.js
@@ -1,5 +1,14 @@
 import _ from 'lodash';
 
 // BEGIN
-
+const fill = (firstObject, keys, secondObject) => {
+    if (keys.length === 0) {
+        for (const props in secondObject) {
+            keys.push(props);
+        }
+    }
+    const pick = _.pick(secondObject, keys);
+    return Object.assign(firstObject, pick);
+}
+export default fill;
 // END
