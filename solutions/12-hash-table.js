9b3ebeace6ab7e6328bf46a4d59eeed3c66e7b26
dima55279 <dima55279@yandex.ru>
2025-03-16 13:54:33 +0300

    Выполнено задание 12. Закончена ЛР 3.

--- solutions/12-hash-table.js
+++ solutions/12-hash-table.js
@@ -1,5 +1,24 @@
 import crc32 from 'crc-32';
 
 // BEGIN
-
+export const make = () => {
+    return [];
+}
+export const set = (map, key, value) => {
+    const hash = crc32.str(key);
+    const index = Math.abs(hash) % 1000;
+    if (map[index] === undefined || map[index][0] === key) {
+        map[index] = [key, value];
+        return true;
+    }
+    return false;
+}
+export const get = (map, key, defaultValue = null) => {
+    const hash = crc32.str(key);
+    const index = Math.abs(hash) % 1000;
+    if (map[index] !== undefined && map[index][0] === key) {
+        return map[index][1];
+    }
+    return defaultValue;
+}
 // END
